b680ad5ef8c4de420395c6b351e394a99868043b
oscar84922tw <oscar84922tw@gmail.com>
2015-11-13 13:47:56 +0800

    debug 1
    Main Tree bug was fixed

--- src/edu/nccu/misdu/stu103306037/hw6/Main.java
+++ src/edu/nccu/misdu/stu103306037/hw6/Main.java
@@ -1,13 +1,14 @@
 package edu.nccu.misdu.stu103306037.hw6;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		WebNode rootNode = new WebNode(new WebPage("Fang Yu",
 				"http://www3.nccu.edu.tw/~yuf"));
-		WebTree tree = new WebTree(rootNode);
+//		WebTree tree = new WebTree(rootNode);
 		rootNode.appendChild(new WebNode(new WebPage("Publication",
 				" http://www3.nccu.edu.tw/~yuf/publication.htm")));
 		rootNode.appendChild(new WebNode(new WebPage("Tool",
@@ -20,24 +21,24 @@ public class Main {
 						new WebPage("OpenWAVES", "http://www.openwaves.net")));
 		rootNode.appendChild(new WebNode(new WebPage("Biography",
 				"http://www3.nccu.edu.tw/~yuf/short_biography.htm")));
-//	繼續複製....
-		
+		// 繼續複製....
+
 		WebTree tree = new WebTree(rootNode);
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		while (sc.hasNextLine()) {
 			int numOfKeywords = sc.nextInt();
 			ArrayList<Keyword> keywords = new ArrayList<>();
-			for(int i = 0;i<numOfKeywords;i++){
+			for (int i = 0; i < numOfKeywords; i++) {
 				String name = sc.next();
 				double weight = sc.nextDouble();
-				keywords.add(new Keyword(name,weight));
-				
+				keywords.add(new Keyword(name, weight));
+
 			}
-	tree.postOrderNodeScore(keywords);
-	tree.eulerTourPrint();
+			tree.postOrderNodeScore(keywords);
+			tree.eulerTourPrint();
 		}
-	
+
 	}
 }
