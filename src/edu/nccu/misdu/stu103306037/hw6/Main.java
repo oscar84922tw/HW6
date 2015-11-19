37dadd9fb59bf5a1aab4a21126836e8be52e3afb
oscar84922tw <oscar84922tw@gmail.com>
2015-11-19 12:37:50 +0800

    Done

--- src/edu/nccu/misdu/stu103306037/hw6/Main.java
+++ src/edu/nccu/misdu/stu103306037/hw6/Main.java
@@ -7,24 +7,47 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		WebNode rootNode = new WebNode(new WebPage("Fang Yu",
-				"http://www3.nccu.edu.tw/~yuf"));
+				"http://www3.nccu.edu.tw/~yuf/"));
 		rootNode.appendChild(new WebNode(new WebPage("Publication",
-				" http://www3.nccu.edu.tw/~yuf/publication.htm")));
+				"http://www3.nccu.edu.tw/~yuf/publications.htm")));
 		rootNode.appendChild(new WebNode(new WebPage("Tool",
 				"http://www3.nccu.edu.tw/~yuf/research_projects_and_tools.htm")));
 		rootNode.children.get(1).appendChild(
-				new WebNode(new WebPage("Stranger",
+				new WebNode(new WebPage("Strangers",
 						"http://www.cs.ucsb.edu/~vlab/stranger/")));
-		rootNode.children.get(1).appendChild(
-				new WebNode(
-						new WebPage("OpenWAVES", "http://www.openwaves.net")));
+		rootNode.children
+				.get(1)
+				.appendChild(
+						new WebNode(
+								new WebPage("The Little Boat",
+										"http://140.119.19.93/data-structure/little-boat-correct.html")));
 		rootNode.appendChild(new WebNode(new WebPage("Biography",
 				"http://www3.nccu.edu.tw/~yuf/short_biography.htm")));
-		rootNode.children.get(2).appendChild(
-				new WebNode(
-						new WebPage("Vlab", " http://www.cs.ucsb.edu/~vlab")));
+		rootNode.children.get(2)
+				.appendChild(
+						new WebNode(new WebPage("Vlab",
+								"http://www.cs.ucsb.edu/~vlab")));
 		rootNode.appendChild(new WebNode(new WebPage("Course",
 				"http://www3.nccu.edu.tw/~yuf/course.htm")));
+		// WebNode rootNode = new WebNode(new WebPage("Fang Yu",
+		// "http://www3.nccu.edu.tw/~yuf"));
+		// rootNode.appendChild(new WebNode(new WebPage("Publication",
+		// " http://www3.nccu.edu.tw/~yuf/publication.htm")));
+		// rootNode.appendChild(new WebNode(new WebPage("Tool",
+		// "http://www3.nccu.edu.tw/~yuf/research_projects_and_tools.htm")));
+		// rootNode.children.get(1).appendChild(
+		// new WebNode(new WebPage("Stranger",
+		// "http://www.cs.ucsb.edu/~vlab/stranger/")));
+		// rootNode.children.get(1).appendChild(
+		// new WebNode(
+		// new WebPage("OpenWAVES", "http://www.openwaves.net")));
+		// rootNode.appendChild(new WebNode(new WebPage("Biography",
+		// "http://www3.nccu.edu.tw/~yuf/short_biography.htm")));
+		// rootNode.children.get(2).appendChild(
+		// new WebNode(
+		// new WebPage("Vlab", " http://www.cs.ucsb.edu/~vlab")));
+		// rootNode.appendChild(new WebNode(new WebPage("Course",
+		// "http://www3.nccu.edu.tw/~yuf/course.htm")));
 
 		WebTree tree = new WebTree(rootNode);
 
@@ -40,7 +63,7 @@ public class Main {
 
 			}
 			tree.postOrderNodeScore(keywords);
-			tree.eulerTourPrint(rootNode);
+			tree.eulerTourPrint();
 		}
 
 	}
--- src/edu/nccu/misdu/stu103306037/hw6/WebTree.java
+++ src/edu/nccu/misdu/stu103306037/hw6/WebTree.java
@@ -1,5 +1,6 @@
 package edu.nccu.misdu.stu103306037.hw6;
 
+//import edu.nccu.misdu.stu103306037.hw6.Main;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -16,15 +17,49 @@ public class WebTree {
 		root.calcNodeScore(keywords);
 	}
 
-	public void eulerTourPrint(WebNode webNode) {
+	public void eulerTourPrint() {
 
-		System.out.print("(" + root.webPage.name + "," +/* 分數計算*/ root.calcNodeScore());
+		System.out.println("(" + root.webPage.name + ","
+				+ /* 分數計算 */root.NodeScore);
 		for (int i = 0; i < root.children.size(); i++) {
-			System.out.println("	" + "(" + root.children.get(i).webPage.name
-					+ "," + /* 分數計算*/ root.calcNodeScore(root.children));
+			System.out.println("\t" + "(" + root.children.get(i).webPage.name
+					+ "," + /* 分數計算 */root.children.get(i).NodeScore);
+			
 			for (int j = 0; j < root.children.get(i).children.size(); j++) {
-			System.out.println("		"+"("+root.children.get(i).children.get(j+","+ /* 分數計算*/ ));
+				System.out.println("\t\t" + "("
+						+ root.children.get(i).children.get(j).webPage.name+ "," + root.children.get(i).webPage.score+")");
 			}
+			System.out.println("\t"+")");
 		}
+		System.out.println(")");
 	}
+
+//	public void eulerTourPrint() {
+//		// print tree
+//		int tabCounter = 0;
+//		if (root.parent != null) {
+//			tabCounter += 1;
+//		}
+//		System.out.print("(" + root.webPage.name + ","
+//				+ root.webPage.score);
+//		if (root.children.size() > 0) {
+//			tabCounter += 1;
+//			for (WebNode child : root.children) {
+//				System.out.println("");
+//				for (int i = 0; i < tabCounter; i++) {
+//					System.out.print("\t");
+//				}
+//				tabCounter = 0;
+//				eulerTourPrint();
+//			}
+//		} else {
+//			System.out.print(")");
+//			return;
+//		}
+//		System.out.println("");
+//		for (int i = 0; i < tabCounter - 1; i++) {
+//			System.out.print("\t");
+//		}
+//		System.out.print(")");
+//	}
 }
